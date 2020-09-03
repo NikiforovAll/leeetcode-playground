540104a4b0d984fecba5a3b0241ee1fd3f2c5f93
NikiforovAll <nikiforovalekcey@gmail.com>
2020-09-03 12:46:30 +0300

    Add 220.contains-duplicate-iii

--- src/LeetCodeSolutions/220.contains-duplicate-iii.cs
+++ src/LeetCodeSolutions/220.contains-duplicate-iii.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 /*
 * @lc app=leetcode id=220 lang=csharp
 *
@@ -51,32 +52,52 @@ namespace _220
 {
 
     // @lc code=start
+    // n log k
     public class Solution
+    {
+        private readonly SortedSet<long> neighbors = new SortedSet<long>();
+
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            if (k < 0 || t < 0) return false;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (IsThereNeighbor(nums[i], t)) return true;
+                neighbors.Add(nums[i]);
+                if (i >= k) neighbors.Remove(nums[i - k]);
+            }
+            return false;
+        }
+
+        private bool IsThereNeighbor(long num, int distance)
+        {
+            return neighbors.GetViewBetween(num - distance, num + distance).Any();
+        }
+    }
+    // @lc code=end
+    // O(k*log k + (n - k)*(k+k))
+    public class Solution2
     {
         public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
         {
             var length = nums.Length - 1;
+            // Span<int> numsSpan = nums;
+            // tmpArr.CopyTo(arr);
             k = length < k ? length : k;
-            // sliding to the right
-            for (int i = 0; i < nums.Length - k; i++)
+            var sorted = nums[..(k + 1)]
+                .OrderBy(i => i)
+                .ToList();
+            for (var i = 0; i < nums.Length - k; i++)
             {
-                // * consider to use insertion sort to optimize
-                var arr = nums[i..(i + k + 1)];
-                GroomArray();
-                // Array.Sort(arr);
-                if (Check(arr, t))
-                    return true;
-                void GroomArray()
+                if (i > 0)
                 {
-                    if (i == 0)
-                    {
-                        Array.Sort(arr);
-                    }
-                    else
-                    {
-                        Insert(arr, nums[i + k]);
-                    }
+                    sorted.Remove(nums[i - 1]);
+                    Insert(sorted, nums[i + k]);
                 }
+                if (Check(sorted, t))
+                    return true;
+
             }
             return false;
 
@@ -88,10 +109,10 @@ namespace _220
         /// <param name="source">Sorted memory</param>
         /// <param name="diff"></param>
         /// <returns></returns>
-        private bool Check(Span<int> source, int diff)
+        private bool Check(IList<int> source, int diff)
         {
             long min = long.MaxValue;
-            for (var i = 0; i < source.Length - 1; i++)
+            for (var i = 0; i < source.Count - 1; i++)
             {
                 var localDiff = (long)source[i] - source[i + 1];
                 localDiff = Math.Abs(localDiff);
@@ -100,27 +121,38 @@ namespace _220
             return min <= diff;
         }
 
-        private void Insert(Span<int> source, int val)
+        private void Insert<T>(IList<T> items, T val)
+            where T : IComparable
         {
-            // var foundInd = source.BinarySearch(
-            //     val,
-            //     Comparer<int>.Create((x, y) => x >= y ? 1 : -1));
             var foundInd = 0;
-            var length = source.Length;
-            while (foundInd < length && source[foundInd] <= val)
+            var length = items.Count;
+            while (foundInd < length && items[foundInd].CompareTo(val) != 1)
             {
                 foundInd++;
             }
-            foundInd--;
-            if(foundInd < length)
-            {
-                source[foundInd..^1].CopyTo(source[(foundInd + 1)..]);
-                source[foundInd] = val;
-            }
-            // Array.Copy(source, foundInd, source, foundInd + 1, source.Length - 1);
+            items.Insert(foundInd, val);
         }
 
+        // in place insertion
+        // private void Insert(Span<int> source, int val)
+        // {
+        //     // var foundInd = source.BinarySearch(
+        //     //     val,
+        //     //     Comparer<int>.Create((x, y) => x >= y ? 1 : -1));
+        //     var foundInd = 0;
+        //     var length = source.Length;
+        //     while (foundInd < length && source[foundInd] <= val)
+        //     {
+        //         foundInd++;
+        //     }
+        //     if (foundInd < length)
+        //     {
+        //         source[foundInd..^1].CopyTo(source[(foundInd + 1)..]);
+        //         source[foundInd] = val;
+        //     }
+        //     // Array.Copy(source, foundInd, source, foundInd + 1, source.Length - 1);
+        // }
+
     }
-    // @lc code=end
 
 }
